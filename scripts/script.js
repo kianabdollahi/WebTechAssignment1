c9c5049c43f02af6868a75ac8af5300a7383522e
Kiki <kabdollahi347@gmail.com>
2023-04-06 15:49:29 -0700

    Creates saveNote function to accept user input, and stores it in array

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,10 @@
+// Declare an array (notesArray) in your JS code with JS note objects in this format: {title:"note one", body:"this is my first note"}.
+// Make sure this variable is declared outside of any function. (1 point)
+// When the user types something in the textarea and clicks the “save” button (4 points)
+// Show a prompt to the user and ask for the title of the note they want to save
+// Add a new JS object to the notesArray with the provided title and the text in the text area as the body
+// Add a list item to the sidebar with the title of the JS object you just added to the array
+
 const themeButton = document.querySelector("#theme-button");
 const buttonContainer = document.querySelector(".note-taking-area");
 const saveButton = document.querySelector("#save-button");
@@ -54,6 +61,28 @@ function clearText(event) {
     }
   }
 }
+
+let notesArray = [{ title: "note one", body: "This is my first note" }];
+
+function saveNote(event) {
+  let noteBody;
+  let noteTitle;
+  let newNote;
+
+  if (event.target.id === "save-button") {
+    if (textArea.value.length != 0) {
+      noteTitle = prompt("Please enter note Title: ");
+      noteBody = textArea.value;
+
+      newNote = { title: `${noteTitle}`, body: `${noteBody}` };
+      notesArray.push(newNote);
+    }
+
+    console.log(notesArray);
+  }
+}
+
 buttonContainer.addEventListener("click", changeTheme);
 buttonContainer.addEventListener("click", hideButtons);
 buttonContainer.addEventListener("click", clearText);
+buttonContainer.addEventListener("click", saveNote);
